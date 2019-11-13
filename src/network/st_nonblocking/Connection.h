b2e7b5b2d194ab7c424b17d375fd57be89a8ff1a
Shevchenko Oleg <Opavlyuchenkov@gmail.com>
2019-11-13 11:27:43 +0300

    First commit for homework #4

--- src/network/st_nonblocking/Connection.h
+++ src/network/st_nonblocking/Connection.h
@@ -4,6 +4,10 @@
 #include <cstring>
 
 #include <sys/epoll.h>
+#include <spdlog/logger.h>
+#include <protocol/Parser.h>
+#include <afina/Storage.h>
+#include <afina/execute/Command.h>
 
 namespace Afina {
 namespace Network {
@@ -11,12 +15,12 @@ namespace STnonblock {
 
 class Connection {
 public:
-    Connection(int s) : _socket(s) {
+    Connection(int s, std::shared_ptr<Afina::Storage> ps) : _socket(s) {
         std::memset(&_event, 0, sizeof(struct epoll_event));
         _event.data.ptr = this;
     }
 
-    inline bool isAlive() const { return true; }
+    inline bool isAlive() const { return is_Alive; }
 
     void Start();
 
@@ -29,8 +33,20 @@ protected:
 private:
     friend class ServerImpl;
 
+    bool is_Alive = true;
+
     int _socket;
     struct epoll_event _event;
+
+    std::shared_ptr<Afina::Storage> pStorage;
+    std::unique_ptr<Execute::Command> command_to_execute;
+    Protocol::Parser parser;
+    std::size_t arg_remains;
+    std::string argument_for_command;
+    std::vector<std::string> answers;
+    int old_readed_bytes = 0;
+
+    ssize_t cur_position = 0;
 };
 
 } // namespace STnonblock
